480f103dc3c78100f1578963239313978471c454
BryanSalim - 535210026 <111833855+bryan777619@users.noreply.github.com>
2022-10-10 02:50:14 +0700

    Update krrs.js

--- krrs.js
+++ krrs.js
@@ -18,3 +18,22 @@ function submitkrrs(){
 
     window.location.href='index1.html';
 }
+function loginkrrs() {
+    var emailmahasiswa = document.getElementById("email").value;
+    var passwordmahasiswa = document.getElementById('password').value;
+    localStorage.setItem("emailvalue", emailmahasiswa);
+    localStorage.setItem("passwordvalue", passwordmahasiswa);
+
+    var mail = localStorage.getItem("emailvalue");
+    var pass = localStorage.getItem("passwordvalue");
+
+    var newmail = localStorage.getItem("newemailvalue");
+    var newpass = localStorage.getItem("newpasswordvalue");
+
+    if(mail == newmail && pass == newpass) {
+        // window.open("index.html");
+        window.location.href="index.html";
+    } else {
+        document.getElementById('akses').innerHTML='Login Failed!';
+    }
+}
